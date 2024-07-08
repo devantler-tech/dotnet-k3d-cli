9183126b2a1a54a5fabc7defe31d3f72217a137d
Nikolai Emil Damm <ned@devantler.com>
2024-07-08 15:25:20 +0200

    chore: Update K3d binary download URLs

--- .github/scripts/get-k3d-binary.sh
+++ .github/scripts/get-k3d-binary.sh
@@ -18,7 +18,7 @@ get() {
   chmod +x "$target_dir/$target_name"
 }
 
-get "https://getbin.io/k3d-io/k3d?os=darwin&arch=amd64" "k3d" "src/KSail/assets/binaries" "k3d-darwin-amd64" false
-get "https://getbin.io/k3d-io/k3d?os=darwin&arch=arm64" "k3d" "src/KSail/assets/binaries" "k3d-darwin-arm64" false
-get "https://getbin.io/k3d-io/k3d?os=linux&arch=amd64" "k3d" "src/KSail/assets/binaries" "k3d-linux-amd64" false
-get "https://getbin.io/k3d-io/k3d?os=linux&arch=arm64" "k3d" "src/KSail/assets/binaries" "k3d-linux-arm64" false
+get "https://getbin.io/k3d-io/k3d?os=darwin&arch=amd64" "k3d" "src/Devantler.K3dCLI/assets/binaries" "k3d-darwin-amd64" false
+get "https://getbin.io/k3d-io/k3d?os=darwin&arch=arm64" "k3d" "src/Devantler.K3dCLI/assets/binaries" "k3d-darwin-arm64" false
+get "https://getbin.io/k3d-io/k3d?os=linux&arch=amd64" "k3d" "src/Devantler.K3dCLI/assets/binaries" "k3d-linux-amd64" false
+get "https://getbin.io/k3d-io/k3d?os=linux&arch=arm64" "k3d" "src/Devantler.K3dCLI/assets/binaries" "k3d-linux-arm64" false
