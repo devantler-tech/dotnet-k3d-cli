74a699af4523d8428d9bd0ec66f918f862116ee1
Nikolai Emil Damm <ned@devantler.com>
2024-07-08 15:17:07 +0200

    feat: Add script to download K3d binary
    This commit adds a new script, `get-k3d-binary.sh`, which is responsible for downloading the K3d binary from different URLs based on the operating system and architecture. The script uses curl to download the binary and places it in the appropriate directory. The downloaded binaries include `k3d-darwin-amd64`, `k3d-darwin-arm64`, `k3d-linux-amd64`, and `k3d-linux-arm64`.
    
    The purpose of this change is to ensure that the K3d binary used by Devantler.AgeCLI is up to date. This will improve the reliability and performance of the application.
    
    Issue #123

---
+++ .github/scripts/get-k3d-binary.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+get() {
+  local url=$1
+  local binary=$2
+  local target_dir=$3
+  local target_name=$4
+  local isTar=$5
+
+  # check if tar
+  if [ "$isTar" = true ]; then
+    curl -LJ "$url" | tar xvz -C "$target_dir" "$binary"
+    mv "$target_dir/$binary" "${target_dir}/$target_name"
+  elif [ "$isTar" = false ]; then
+    curl -LJ "$url" -o "$target_dir/$target_name"
+  fi
+  chmod +x "$target_dir/$target_name"
+}
+
+get "https://getbin.io/k3d-io/k3d?os=darwin&arch=amd64" "k3d" "src/KSail/assets/binaries" "k3d-darwin-amd64" false
+get "https://getbin.io/k3d-io/k3d?os=darwin&arch=arm64" "k3d" "src/KSail/assets/binaries" "k3d-darwin-arm64" false
+get "https://getbin.io/k3d-io/k3d?os=linux&arch=amd64" "k3d" "src/KSail/assets/binaries" "k3d-linux-amd64" false
+get "https://getbin.io/k3d-io/k3d?os=linux&arch=arm64" "k3d" "src/KSail/assets/binaries" "k3d-linux-arm64" false
